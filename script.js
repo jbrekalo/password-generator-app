fc36f9d1d7b42a185da88d8053b85543bd4cb517
jbrekalo <josip.brekalo@hotmail.com>
2023-06-28 14:37:12 +0200

    Strength rating done

--- script.js
+++ script.js
@@ -10,103 +10,111 @@ const checkLower = document.getElementById("lowercase");
 const checkNumber = document.getElementById("numbers");
 const checkSymbol = document.getElementById("symbols");
 
+const strengthBars = document.querySelectorAll(".app__strength-bar");
+
 const btnGenerate = document.querySelector(".app__button");
 const btnCopy = document.querySelector(".app__icon-copy");
 
+const checkboxes = [checkUpper, checkLower, checkNumber, checkSymbol];
 let charLength = 10;
-let strengthPoints = 5;
+let pointsCheckboxes = 5;
+let pointsSlider = 0;
+let pointsTotal = 0;
 
 const passGenerator = function () {
   let result = "";
-  let chars = "";
+  let charactersSum = "";
 
-  const charUppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const charLowercase = "abcdefghijklmnopqrstuvwxyz";
-  const charNumbers = "0123456789";
-  const charSymbols = "!@#&()–[{}]:;',?/*`~$^+=<>";
+  const characters = [
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "abcdefghijklmnopqrstuvwxyz",
+    "0123456789",
+    "!@#&()-[{}]:;',?/*`~$^+=<>",
+  ];
 
-  if (checkUpper.checked) {
-    chars += charUppercase;
-  }
-
-  if (checkLower.checked) {
-    chars += charLowercase;
-  }
-
-  if (checkNumber.checked) {
-    chars += charNumbers;
-  }
-
-  if (checkSymbol.checked) {
-    chars += charSymbols;
-  }
+  checkboxes.forEach((checkbox, i) => {
+    if (checkbox.checked) {
+      charactersSum += characters[i];
+    }
+  });
 
   let counter = 0;
   while (counter < charLength) {
-    result += chars.charAt(Math.floor(Math.random() * chars.length));
+    result += charactersSum.charAt(
+      Math.floor(Math.random() * charactersSum.length)
+    );
     counter += 1;
   }
 
   return result;
 };
 
-const strengthRating = function () {};
+const resetRating = function () {
+  strengthBars.forEach((bar) => {
+    bar.classList.remove("app__strength-bar--very-weak");
+    bar.classList.remove("app__strength-bar--weak");
+    bar.classList.remove("app__strength-bar--medium");
+    bar.classList.remove("app__strength-bar--strong");
+  });
+};
+
+const strengthRating = function () {
+  pointsTotal = pointsSlider + pointsCheckboxes;
+
+  if (pointsTotal <= 14.5) {
+    resetRating();
+    for (let i = 0; i < strengthBars.length - 3; i++) {
+      const bar = strengthBars[i];
+      console.log(bar);
+      bar.classList.add("app__strength-bar--very-weak");
+    }
+  } else if (pointsTotal >= 14.5 && pointsTotal < 23) {
+    resetRating();
+    for (let i = 0; i < strengthBars.length - 2; i++) {
+      const bar = strengthBars[i];
+      bar.classList.add("app__strength-bar--weak");
+    }
+  } else if (pointsTotal >= 23 && pointsTotal < 31.5) {
+    resetRating();
+    for (let i = 0; i < strengthBars.length - 1; i++) {
+      const bar = strengthBars[i];
+      bar.classList.add("app__strength-bar--medium");
+    }
+  } else if (pointsTotal >= 31.5) {
+    resetRating();
+    for (let i = 0; i < strengthBars.length; i++) {
+      const bar = strengthBars[i];
+      bar.classList.add("app__strength-bar--strong");
+    }
+  }
+};
 
 sliderEl.addEventListener("input", function (e) {
   const tempSliderValue = e.target.value;
   sliderValue.textContent = tempSliderValue;
 
-  charLength = tempSliderValue;
+  charLength = Number(tempSliderValue);
+  pointsSlider = Number(tempSliderValue);
+
+  strengthRating();
 
   const progress = ((tempSliderValue - 1) / 19) * 100;
   sliderProgressBar.style.width = `${progress}%`;
-  console.log(progress);
 });
 
 checks.forEach((el) =>
   el.addEventListener("click", function () {
-    // let cb1, cb2, cb3, cb4, cbSum;
-    const checkboxes = [
-      { checkbox: checkUpper, value: 5 },
-      { checkbox: checkLower, value: 5 },
-      { checkbox: checkNumber, value: 5 },
-      { checkbox: checkSymbol, value: 5 },
-    ];
-    let cbSum = 0;
+    pointsCheckboxes = 0;
 
-    // if (checkUpper.checked) {
-    //   cb1 = 5;
-    // } else {
-    //   cb1 = 0;
-    // }
-
-    // if (checkLower.checked) {
-    //   cb2 = 5;
-    // } else {
-    //   cb2 = 0;
-    // }
-
-    // if (checkNumber.checked) {
-    //   cb3 = 5;
-    // } else {
-    //   cb3 = 0;
-    // }
-
-    // if (checkSymbol.checked) {
-    //   cb4 = 5;
-    // } else {
-    //   cb4 = 0;
-    // }
-
-    // cbSum = cb1 + cb2 + cb3 + cb4;
-
-    checkboxes.forEach(({ checkbox, value }) => {
+    checkboxes.forEach((checkbox) => {
       if (checkbox.checked) {
-        cbSum += value;
+        pointsCheckboxes += Number(checkbox.value);
       }
     });
 
-    console.log(cbSum);
+    strengthRating();
+
+    console.log(pointsCheckboxes);
   })
 );
 
